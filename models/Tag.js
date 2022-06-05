83c4dccbde5248625ba1d811d508ec08c7b07063
Luiz Medina <luizmedina87@gmail.com>
2022-06-05 17:12:16 -0400

    added models and corrected connection

--- models/Tag.js
+++ models/Tag.js
@@ -1,20 +1,36 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
 module.exports = Tag;
+
+// id
+// Integer
+// Doesn't allow null values
+// Set as primary key
+// Uses auto increment
+// tag_name
+// String
